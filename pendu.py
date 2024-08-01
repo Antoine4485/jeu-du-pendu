a3127079b350bcbc11830f2e4df1928785063519
Antoine4485 <pzx.antoine@gmail.com>
2024-08-01 09:13:06 +0200

    # avec classe Hanged

--- pendu.py
+++ pendu.py
@@ -1,118 +1,121 @@
+import random
 import string
 from turtle import *
-import random
 
 
-def est_lettre_valide(lettre):
-    if lettre in string.ascii_lowercase:
-        return True
-    return False
+class Hanged:
 
+    def __init__(self):
+        self.word = self._get_word()
+        self._play()
 
-def est_mot_valide(mot):
-    for lettre in mot:
-        if not est_lettre_valide(lettre):
-            return False
-    return True
+    def _get_word(self):
+        file = open("dictionnaire.txt", "r")
+        lines = file.readlines()
 
+        # Choisir un mot valide dans le dictionnaire
+        while True:
+            word = random.choice(lines).strip().lower()
+            if self._is_valid_word(word):
+                return word
 
-def affiche_element_pendu(nb_erreurs):
-    match nb_erreurs:
-        case 1:
-            forward(120)
-        case 2:
-            backward(60)
-            left(90)
-            forward(70)
-        case 3:
-            forward(70)
-        case 4:
-            forward(70)
-        case 5:
-            right(90)
-            forward(80)
-        case 6:
-            right(90)
-            forward(30)
-        case 7:
-            right(90)
-            forward(0.1)
-            circle(10)
-            up()
-            sety(ycor() - 20)
-            left(90)
-            down()
-            # haut du tronc
-            forward(10)
-            # bras droit
-            left(90)
-            forward(20)
-            # bras gauche
-            up()
-            right(180)
-            setx(xcor() - 20)
-            down()
-            forward(20)
-            # bas du tronc
-            up()
-            left(90)
-            setx(xcor() + 20)
-            down()
-            forward(25)
-            x, y = pos()
-            # jambe gauche
-            right(30)
-            forward(30)
-            # jambe droite
-            up()
-            setpos(x, y)
-            left(60)
-            down()
-            forward(30)
+    def _play(self):
+        response_letters = ["-" for _ in self.word]
+        nb_errors = 0
+        max_nb_errors = 7
 
-
-def get_word():
-    file = open("dictionnaire.txt", "r")
-    lines = file.readlines()
-
-    # Choisir un mot valide dans le dictionnaire
-    while True:
-        word = random.choice(lines).strip().lower()
-        if est_mot_valide(word):
-            return word
-
-
-def play():
-    word = get_word()
-    tab_lettres_reponse = ["-" for _ in word]
-    nb_tries = 0
-    nb_tries_max = 7
-
-    while True:
-        letter = input("Saisir une lettre sans accent : ")
-        if not est_lettre_valide(letter):
-            print("Cette letter n'est pas valide")
-            continue
-        if letter not in word:
-            nb_tries += 1
-            affiche_element_pendu(nb_tries)
-            if nb_tries < nb_tries_max:
+        while True:
+            letter = input("Saisir une lettre sans accent : ")
+            if not self._is_valid_letter(letter):
+                print("Lettre non valide")
                 continue
-            print(f"Partie perdue, le mot à trouver était : {word}")
-            break
-        else:
+            if letter not in self.word:
+                nb_errors += 1
+                self._show_hanged_element(nb_errors)
+                if nb_errors < max_nb_errors:
+                    continue
+                print(f"\nPartie perdue, le mot à trouver était : {self.word}")
+                break
             # on affiche la lettre aux emplacements où elle se trouve dans le mot à chercher
-            for i in range(len(word)):
-                if word[i] == letter:
-                    tab_lettres_reponse[i] = letter
-            mot_reponse = "".join(tab_lettres_reponse)
-            print(mot_reponse)
-            if mot_reponse == word:
-                print("Partie gagnée !")
+            for i in range(len(self.word)):
+                if self.word[i] == letter:
+                    response_letters[i] = letter
+            response_word = "".join(response_letters)
+            print(response_word)
+            if response_word == self.word:
+                print("\nPartie gagnée !")
                 break
 
-    input()
+        input()
+
+    @staticmethod
+    def _is_valid_letter(letter):
+        if letter in string.ascii_lowercase:
+            return True
+        return False
+
+    @staticmethod
+    def _is_valid_word(word):
+        for letter in word:
+            if not Hanged._is_valid_letter(letter):
+                return False
+        return True
+
+    @staticmethod
+    def _show_hanged_element(nb_errors):
+        match nb_errors:
+            case 1:
+                forward(120)
+            case 2:
+                backward(60)
+                left(90)
+                forward(70)
+            case 3:
+                forward(70)
+            case 4:
+                forward(70)
+            case 5:
+                right(90)
+                forward(80)
+            case 6:
+                right(90)
+                forward(30)
+            case 7:
+                right(90)
+                forward(0.1)
+                circle(10)
+                up()
+                sety(ycor() - 20)
+                left(90)
+                down()
+                # haut du tronc
+                forward(10)
+                # bras droit
+                left(90)
+                forward(20)
+                # bras gauche
+                up()
+                right(180)
+                setx(xcor() - 20)
+                down()
+                forward(20)
+                # bas du tronc
+                up()
+                left(90)
+                setx(xcor() + 20)
+                down()
+                forward(25)
+                x, y = pos()
+                # jambe gauche
+                right(30)
+                forward(30)
+                # jambe droite
+                up()
+                setpos(x, y)
+                left(60)
+                down()
+                forward(30)
 
 
 if __name__ == '__main__':
-    play()
+    Hanged()
